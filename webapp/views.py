1bd78e2dba255557eb0a4063b738fc81092b7adb
sivadinesh1 <sivadinesh@gmail.com>
2018-02-02 11:52:38 +0530

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -4,20 +4,26 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
+#from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from . models import courses
 from . serializers import coursesSerializer
 
-class coursesList(APIView):
+@api_view(['GET', 'POST'])
+def courses_list(request):
 
-    def get(self, request):
+    if request.method == 'GET':
         coursestemp = courses.objects.all();
         serializer = coursesSerializer(coursestemp, many=True)
         return Response(serializer.data)
 
-    def post(self):
-        pass
-
-
+    elif request.method == 'POST':
+        serializer = coursesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
